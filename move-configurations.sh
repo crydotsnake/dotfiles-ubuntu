9182ff7b2b9ea308f7138245ff8f01ff54c0e0bc
Simon Krull <simonkrull@mailbox.org>
2023-08-08 17:46:59 +0200

    TASK: adjust scripts

--- move-configurations.sh
+++ move-configurations.sh
@@ -17,11 +17,11 @@ fi
 
 if [[ -d "$i3StatusConfigurationPath" ]]; then
 	echo "I will move the i3status configuration too the needed location."
-	cp i3status/i3status.conf $i3StatusConfigurationPath/i3status.conf
+	cp -r i3status/i3status.conf $i3StatusConfigurationPath/i3status.conf
 	echo "Done"
 else 
 	echo "The Path $i3StatusConfigurationPath doesent exist. Will create it now."
 	mdkir $i3StatusConfigurationPath
-	cp i3status/i3status.conf $i3StatusConfigurationPath/i3status.conf
+	cp -r i3status/i3status.conf $i3StatusConfigurationPath/i3status.conf
 	echo "Done"
 fi
