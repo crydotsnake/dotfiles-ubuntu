6a55ce5f3aa7e4c399b28c33da3e21392196c28b
Simon Krull <simonkrull@mailbox.org>
2023-08-19 02:58:45 +0200

    TASK: add information about i3 repository site

--- install-packages.sh
+++ install-packages.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 sudo apt update && sudo apt upgrade
-sudo apt install i3 feh arandr pasystray rofi lxappearance -y
+sudo apt install terminator feh arandr pasystray rofi lxappearance -y
 
+echo "i3 install instructions at: https://i3wm.org/docs/repositories.html"
+echo "Also dont forget too take a look at: https://forums.linuxmint.com/viewtopic.php?t=388673"
